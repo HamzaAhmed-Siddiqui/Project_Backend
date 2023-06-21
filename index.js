ee605601180c40ac4f079807ca08d548416a3b44
hamza <hamza.ahmed@eocean.com.pk>
2023-06-21 18:33:30 +0500

    Latest

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const connection = require("./Database");
 const express = require("express");
 
 var app = express();
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
 
 app.get("/", (req, res) => {
     res.json({ message: "ok" });
@@ -11,8 +16,68 @@ app.get("/", (req, res) => {
 
 app.use(bodyParser.json());
 
-app.get("/user", (req, res) => {
-    connection.query("SELECT * FROM users", (err, rows) => {
+// app.get("/login", (req, res) => {
+//     connection.query(
+//       "SELECT * FROM users " +
+//       "UNION " +
+//       "SELECT * FROM teachers",
+//       (err, rows) => {
+//         if (err) {
+//           console.log(err); // Log the error details
+//           res.status(500).send("Internal Server Error");
+//         } else {
+//           res.send(rows);
+//         }
+//       }
+//     );
+//   });
+
+//   app.get('/students', (req, res) => {
+//     res.json(students);
+//   });
+//   app.post('/login', (req, res) => {
+//     const { email, password } = req.body;
+//     const student = students.find(
+//       student => student.email === email && student.password === password
+//     );
+//     if (student) {
+//       res.json({ message: 'Login successful' });
+//     } else {
+//       res.status(401).json({ message: 'Invalid credentials' });
+//     }
+//   });
+
+  app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    connection.query(
+    //   "SELECT * FROM users " +
+    //   "UNION " +
+      "SELECT * FROM teachers",
+      (err, rows) => {
+        if (err) {
+          console.log(err); // Log the error details
+          res.status(500).send("Internal Server Error");
+        } else {
+          const student = rows.find(
+            row => row.email === email && row.password === password
+          );
+          if (student) {
+            res.json({ message: 'Login successful' });
+          } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+          }
+        }
+      }
+    );
+
+  });
+  
+
+
+
+
+app.get("/teacher", (req, res) => {
+    connection.query("SELECT * FROM teachers", (err, rows) => {
         if (err) {
             console.log(err);
         } else {
@@ -21,6 +86,8 @@ app.get("/user", (req, res) => {
         }
     });
 });
+
+
 
 app.get("/user/:id", (req, res) => {
     connection.query(
@@ -52,12 +119,30 @@ app.delete("/user/:id", (req, res) => {
         }
     );
 });
-//name class email password 
 
-app.post("/teacher", async (req, res) => {
+app.delete("/teacher/:id", (req, res) => {
+    connection.query(
+        "DELETE FROM teachers WHERE teacher_id=?",
+        [req.params.id],
+        (err, rows) => {
+            if (err) {
+                console.log(err);
+            } else {
+                //console.log(rows)
+                res.send(rows);
+                console.log("Success")
+            }
+        }
+    );
+});
+
+//name class email password 
+//SIGNUP API
+app.post("/teacherSignup", async (req, res) => {
     try {
         const emp = req.body;
         const empData = [emp.name, emp.class, emp.email, emp.password];
+
 
         // Check if the name already exists in the database
         connection.query(
@@ -72,6 +157,7 @@ app.post("/teacher", async (req, res) => {
                 if (rows.length > 0) {
                     // Name already exists in the database
                     return res.status(400).send({ message: "Name already exists" });
+                    res.json({message:"name already in db"})
                 }
 
                 // Insert the new record
@@ -97,4 +183,4 @@ app.post("/teacher", async (req, res) => {
 
 
 
-app.listen(6000, () => console.log("Server Started"));
+app.listen(3000, () => console.log("Server Started"));
